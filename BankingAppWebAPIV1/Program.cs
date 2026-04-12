2fbdf844f42dda4ddb4387730f3b566048bfc3c1
Steven Riaan Piek <milkx00@gmail.com>
2026-04-12 22:43:25 +0200

    Added CORS policy (AllowAll) and enabled middleware to fix Swagger 'Failed to fetch' error

--- BankingAppWebAPIV1/Program.cs
+++ BankingAppWebAPIV1/Program.cs
@@ -1,45 +1,51 @@
-//I transformed my console-based banking system into a RESTful ASP.NET Core Web API,
-//implementing controllers, services, and repositories with Entity Framework Core and SQL
-//Server for persistent data storage, user authentication, and transaction handling.
+// I transformed my console-based banking system into a RESTful ASP.NET Core Web API,
+// implementing controllers, services, and repositories with Entity Framework Core and SQL
+// Server for persistent data storage, user authentication, and transaction handling.
 
 using BankingAppWebAPIV1.Data;
 using Microsoft.EntityFrameworkCore;
 using BankingAppWebAPIV1.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 
-
 builder.Services.AddEndpointsApiExplorer();
-
-
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<UserService>();
-
 
 var app = builder.Build();
 
 
 app.UseSwagger();
-
-
 app.UseSwaggerUI();
 
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-
 app.MapControllers();
-
 
 app.Run();
